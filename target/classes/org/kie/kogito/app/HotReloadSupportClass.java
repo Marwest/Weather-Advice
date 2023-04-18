3e630f6ad926fa0cfb4b4907d5c939da7990e077
Marwest <martin_westphal@web.de>
2023-04-18 03:21:31 -0700

    First commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "6ec6ea01-d08e-4a11-9e17-78f11ac789d9";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "58c66e8e-a482-48ea-9223-587e13023c07";
+}
